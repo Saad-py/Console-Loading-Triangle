2c9ab7dfad6b0fa6e433f393cbed07375bb06877
Saad-py <73306005+Saad-py@users.noreply.github.com>
2021-02-01 22:12:42 +0400

    MAIN File
    Done in CPP or G++ or C++

---
+++ main.py
@@ -0,0 +1,113 @@
+# Made by Saad-py
+
+# BRUH don't plairigizee
+
+"""
+
+Any Ways........................... FBI open up
+
+Subscibe my YT, follow my github
+
+Programming go Brrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr.
+
+
+"""
+
+
+
+
+from time import sleep
+import os as os
+
+def main():
+  
+
+  a = [
+    '  *  ',
+    ' * * ',
+    '* * *'
+  ]
+
+  n = [
+    '  *  ',
+    ' * * ',
+    '* * *'
+  ]
+
+  def pl():
+    for i in a:
+      print(i)
+    # print("\n")
+
+  def c():
+    sleep(0.10)
+    os.system("cls")
+
+
+  pl()
+  c()
+  a[2] = "* * "
+
+
+  pl()
+  c()
+  a[2] = "  * "
+
+  pl()
+  c()
+  a[2] = ""
+
+
+  pl()
+  c()
+  a[1] = " *"
+
+  pl()
+  c()
+  a[1] = ""
+
+  pl()
+  c()
+  a[0] = " "
+  
+  pl()
+  c()
+  a[2] = "*"
+
+  pl()
+  c()
+  a[2] = "* *"
+
+  pl()
+  c()
+  a[2] = "* * *"
+
+  pl()
+  c()
+  a[1] = " *"
+
+  pl()
+  c()
+  a[1] = " * * "
+
+  pl()
+  c()
+  a[0] = "  *  "
+
+  pl()
+  c()
+
+# running it infinitly using while loop
+while True:
+  main()
+
+
+
+"""
+I used sleep from time and the os module to clear the output state
+
+View of code
+
+I had some sketches so I will roll them in
+"""
+
